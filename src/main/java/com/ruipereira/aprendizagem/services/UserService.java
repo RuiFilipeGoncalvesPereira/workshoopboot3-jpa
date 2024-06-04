9a8f90d8a79dc6f0e07b758ba5308a169f518ec7
Rui Pereira <rui6.319@hotmail.com>
2024-06-04 12:05:27 +0100

    Service layer, component registration

---
+++ src/main/java/com/ruipereira/aprendizagem/services/UserService.java
@@ -0,0 +1,31 @@
+package com.ruipereira.aprendizagem.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ruipereira.aprendizagem.entities.Utilizador;
+import com.ruipereira.aprendizagem.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+
+	public List<Utilizador> findAll()
+	{
+		return repository.findAll();
+	}
+	
+	public Utilizador findById(Long id)
+	{
+		Optional<Utilizador> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
