6a7abc15dace16c8cebf098a799c73864091c038
Rui Pereira <rui6.319@hotmail.com>
2024-06-20 22:59:25 +0100

    Payment, one-to-one association

--- src/main/java/com/ruipereira/aprendizagem/Config/TestConfig.java
+++ src/main/java/com/ruipereira/aprendizagem/Config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ruipereira.aprendizagem.entities.Category;
 import com.ruipereira.aprendizagem.entities.Order;
 import com.ruipereira.aprendizagem.entities.OrderItem;
+import com.ruipereira.aprendizagem.entities.Payment;
 import com.ruipereira.aprendizagem.entities.Product;
 import com.ruipereira.aprendizagem.entities.Utilizador;
 import com.ruipereira.aprendizagem.entities.enums.OrderStatus;
@@ -78,9 +79,11 @@ public class TestConfig implements CommandLineRunner{
 	    OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 	    
 	    orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
-
 	    
+        Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1); 
+        o1.setPayment(pay1);
 	    
+        orderRepository.save(o1);
 	}
 	
 }
