787e86cbfb5f2348e68cb801105eef9c0ad70c0b
Rui Pereira <rui6.319@hotmail.com>
2024-06-24 10:43:44 +0100

    Exception handling - delete

--- src/main/java/com/ruipereira/aprendizagem/services/UserService.java
+++ src/main/java/com/ruipereira/aprendizagem/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ruipereira.aprendizagem.entities.Utilizador;
 import com.ruipereira.aprendizagem.repositories.UserRepository;
+import com.ruipereira.aprendizagem.services.exceptions.DatabaseException;
 import com.ruipereira.aprendizagem.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -35,7 +38,14 @@ public class UserService {
 	
 	public void delete(Long id)
 	{
-		repository.deleteById(id);
+		try {
+		 repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e)
+		{
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public Utilizador update(Long id, Utilizador obj)
